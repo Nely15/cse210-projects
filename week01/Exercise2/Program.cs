495975810dcd6902d4e6f3eb4042f344f2cc2296
Nely15 <bruiz2@byupathway.edu>
2026-05-09 16:05:41 -0500

    week01 exercise2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,82 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("Enter grade number: ");
+        string valueFromUser = Console.ReadLine();
+        int percent = int.Parse(valueFromUser);
+
+        string letter = "";
+
+        if (percent >= 93)
+        {
+            letter = "A";
+        }
+
+        else if (percent >= 90) 
+        {
+            letter = "A-";
+        }
+
+        else if (percent >= 87)
+        {
+            letter = "B+";
+        }
+
+        else if (percent >= 83)
+        {
+            letter = "B";
+        }
+
+        else if (percent >= 80)
+        {
+            letter = "B-";
+        }
+
+        else if (percent >= 77)
+        {
+            letter = "C+";
+        }
+
+        else if (percent >= 73) 
+        {
+            letter = "C";
+        }
+
+        else if (percent >= 70) 
+        {
+            letter = "C-";
+        }
+
+        else if (percent >= 67)
+        {
+            letter = "D+";
+        }
+
+        else if (percent >= 63) 
+        {
+            letter = "D";
+        }
+
+        else if (percent >= 60) 
+        {
+            letter = "D-";
+        }
+
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is {letter}");
+
+        if (percent >= 70)
+        {
+            Console.WriteLine("Great job, you passed!");
+        }
+
+        else
+        {
+            Console.WriteLine("Sorry, you didn't pass. Keep trying! ");
+        }
     }
 }
